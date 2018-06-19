79bb4227666068a2b2cf56125d8578200145b345
Reginald T Carter iI <reginaldcarterii@gmail.com>
2018-06-19 23:23:24 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,7 +13,7 @@ end
 def assign_rooms(attendees)
   room = 1
   attendees.each do |speaker|
-    "Hello,#{speaker}! You'll be assigned to room #{room}!"
     room += 1
   end
+  "Hello,#{speaker}! You'll be assigned to room #{room}!"
 end
